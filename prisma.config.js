72c3f8ca915be2e2c633d369bc6e759a6435222a
FizaWaseem <wfiza842@gmail.com>
2026-07-23 21:48:31 +0500

    Remove unused Prisma config files

--- prisma.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  datasource: {
-    url: "postgresql://postgres.zwvseidfekoppcelujvq:%40iahm2J%23zNAUVJB@aws-1-ap-southeast-1.db.supabase.com:5432/postgres",
-  },
-}
--- prisma/prisma.config.ts
+++
@@ -1,3 +0,0 @@
-export const datasource = {
-  url: process.env.DATABASE_URL,
-}
